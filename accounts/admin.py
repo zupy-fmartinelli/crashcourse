47e5eda237bff8a76639221dcdc8358ce96bcd65
Fabio Martinelli <webmaster@zupy.com.br>
2022-08-20 18:21:30 -0300

    Added Models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# from .models import Customer # Opção para importar apenas um model
+from .models import * #coringa para importar todos os modelos de uma vez
+
+# Aqui é a opção para os modelos aparecerem na administração do Django
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
