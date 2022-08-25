b89ad1f4146212d8149a8459dc649218766c02ab
Fabio Martinelli <webmaster@zupy.com.br>
2022-08-25 20:32:00 -0300

    inline formset

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here.
 from .models import *
 from .forms import OrderForm
@@ -31,18 +32,21 @@ def customer(request, pk_test):
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request,'accounts/customer.html', context)
 
-def createOrder(request):
-    
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    #form = OrderForm(initial={'customer':customer})
     # Pega as informações enviadas no formulário, verifica se é valido, redireciona e salva
     if request.method == 'POST':
         # print('Printing post:', request.POST) # Verifica o que foi enviado no formulario
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
     
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
